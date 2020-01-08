125cfbfacb82eff4c8e7917673c31859c3d23ce0
SultanKaya <zasultan80@gmail.com>
2020-01-08 17:13:08 -0600

    myFirstCucumberProject

--- src/test/java/com/Odoo/Pages/CreateVendorPage.java
+++ src/test/java/com/Odoo/Pages/CreateVendorPage.java
@@ -3,45 +3,42 @@ package com.Odoo.Pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
+
 public class CreateVendorPage {
 
     @FindBy(xpath = "//span[@class='oe_menu_text' and contains(text(),'Purchases')]")
 
-    WebElement purchaseButton;
+   public WebElement purchaseButton;
 
     @FindBy(xpath = "//span[@class='oe_menu_text' and contains(text(),'Vendors')]")
-    WebElement vendorsButton;
+    public WebElement vendorsButton;
 
     @FindBy(xpath= "//button[@class='btn btn-primary btn-sm o-kanban-button-new btn-default' and contains(text(),'Create')]")
-    WebElement createButton;
+   public WebElement createButton;
 
-    @FindBy(id= "o_field_input_1811")
-    WebElement nameBox;
+    @FindBy(xpath= "//*[starts-with(@class,'o_field_char o_field_widget o_input')]")
+    public WebElement nameBox;
 
     @FindBy (xpath="//input[@class='o_field_char o_field_widget o_input o_address_street']")
-    WebElement streetBox;
+   public WebElement streetBox;
 
-    @FindBy(id= "o_field_input_2061")
-    WebElement cityButton;
+    @FindBy(xpath = "//*[contains(@class,'o_field_widget o_input o_address_city')]")
+   public  WebElement cityButton;
 
-    @FindBy (id= "o_field_input_2062")
-    WebElement stateButton;
+    @FindBy (xpath= "//*[contains(@placeholder,'S\uFEFFt\uFEFFa\uFEFFt\uFEFFe')]")
+   public  WebElement stateButton;
 
-    @FindBy( id = "o_field_input_2063")
-    WebElement zipButton;
+    @FindBy( xpath = "//input[contains(@name,'zip')]")
+   public  WebElement zipButton;
 
     @FindBy( xpath = "//input[@name='mobile']")
-    WebElement mobileButton;
+   public WebElement mobileButton;
 
     @FindBy(xpath = "//*[@class='o_field_email o_field_widget o_input']")
-    WebElement emailButton;
+   public WebElement emailButton;
 
-    @FindBy(xpath="//input[@id='o_field_input_2073']")
-    WebElement titledropDown;
-
-
-
-
+    @FindBy (xpath = "//button[@class='btn btn-primary btn-sm o-kanban-button-new' and contains(text(),'Create')]")
+    public WebElement lastCreateButton;
 
 
 
--- src/test/java/com/Odoo/step_definitions/CreateVendorStepDefinitions.java
+++ src/test/java/com/Odoo/step_definitions/CreateVendorStepDefinitions.java
@@ -1,51 +1,76 @@
 package com.Odoo.step_definitions;
 
+import com.Odoo.Pages.CreateVendorPage;
+import com.Odoo.Pages.LoginPage;
+import com.Odoo.utilities.BrowserUtils;
+import com.Odoo.utilities.ConfigurationReader;
+import com.Odoo.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 
 public class CreateVendorStepDefinitions {
 
-
+    CreateVendorPage newVendor = new CreateVendorPage();
+    LoginPage login = new LoginPage();
 
     @Given("User is on the log in page")
     public void user_is_on_the_log_in_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("I am on the login page");
+        Driver.get().get(ConfigurationReader.getProperty("url"));
     }
 
     @Given("User logs in….")
     public void user_logs_in() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("Login as  posmanager");
+        String userName = ConfigurationReader.getProperty("username");
+        BrowserUtils.wait(5);
+        String password = ConfigurationReader.getProperty("password");
+        login.login(userName, password);
+        BrowserUtils.wait(5);
     }
 
     @Then("User navigates to “Purchases” then to “vendors”")
     public void user_navigates_to_Purchases_then_to_vendors() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        newVendor.purchaseButton.click();
+        BrowserUtils.wait(6);
+        newVendor.vendorsButton.click();
+        BrowserUtils.wait(6);
+
     }
 
     @Then("User clicks on create button")
     public void user_clicks_on_create_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        newVendor.createButton.click();
+        BrowserUtils.wait(6);
     }
 
     @Then("User adds new vendor  information")
     public void user_adds_new_vendor_information() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+        newVendor.nameBox.sendKeys("Sophie Kayla");
+        BrowserUtils.wait(6);
+        newVendor.streetBox.sendKeys("263 Pony dr.");
+        BrowserUtils.wait(6);
+        newVendor.cityButton.sendKeys("Dallas");
+        BrowserUtils.wait(6);
+        newVendor.stateButton.sendKeys("Texas");
+        BrowserUtils.wait(6);
+        newVendor.zipButton.sendKeys("75002");
+        BrowserUtils.wait(6);
+        newVendor.mobileButton.sendKeys("469 645 8354");
+        BrowserUtils.wait(6);
+        newVendor.emailButton.sendKeys("jdjbnc26@gmail.com");
+        BrowserUtils.wait(6);
+
     }
 
     @Then("User clicks create button")
     public void user_clicks_create_button() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+
+        newVendor.lastCreateButton.click();
+        BrowserUtils.wait(6);
+        System.out.println("New Vendor is created");
     }
-
-
-
-
 
 
 }
