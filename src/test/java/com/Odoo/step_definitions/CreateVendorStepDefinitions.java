62911fa9c6c952ac94b11bc0bc24ca17e90f857a
SultanKaya <zasultan80@gmail.com>
2020-01-07 21:24:12 -0600

    myFirstCucumberProject

---
+++ src/test/java/com/Odoo/step_definitions/CreateVendorStepDefinitions.java
@@ -0,0 +1,51 @@
+package com.Odoo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+public class CreateVendorStepDefinitions {
+
+
+
+    @Given("User is on the log in page")
+    public void user_is_on_the_log_in_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Given("User logs in….")
+    public void user_logs_in() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("User navigates to “Purchases” then to “vendors”")
+    public void user_navigates_to_Purchases_then_to_vendors() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("User clicks on create button")
+    public void user_clicks_on_create_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("User adds new vendor  information")
+    public void user_adds_new_vendor_information() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+    @Then("User clicks create button")
+    public void user_clicks_create_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new cucumber.api.PendingException();
+    }
+
+
+
+
+
+
+}
